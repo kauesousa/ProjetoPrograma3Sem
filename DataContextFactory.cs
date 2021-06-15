1cc4ef7676db26b72d58393c3f9b777b112be76e
kauesousa <68699884+kauesousa@users.noreply.github.com>
2021-06-15 09:47:25 -0300

    Add files via upload

---
+++ DataContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaudeDay.DAL
+{
+    public class DataContextFactory
+    {
+        private static SaudeDayDataContext dataContext;
+        public static SaudeDayDataContext DataContext
+        {
+            get
+            {
+                if (dataContext == null)
+                    dataContext = new SaudeDayDataContext();
+
+                return dataContext;
+            }
+        }
+    }
+}
